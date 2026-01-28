f25a238ee58b73b635b9996002542f054b983abe
aadinathsj <asanjee@rockets.utoledo.edu>
2026-01-27 22:42:04 -0500

    Added RH Arrow Favicon - Aadi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,16 @@ export const metadata: Metadata = {
   title: "RocketHacks 2026 | Hack the Horizon",
   description:
     "RocketHacks is a 24-hour Hackathon hosted by The University of Toledo. Join us in 2026 for the biggest hackathon in the Midwest!",
-  keywords: "hackathon, programming, technology, university of toledo, coding, innovation",
+  keywords:
+    "hackathon, programming, technology, university of toledo, coding, innovation",
   authors: [{ name: "RocketHacks Team" }],
+  icons: {
+    icon: "/assets/rh_26/rh_26_folder/rh_26_bundle_png/rh_26_arrow_transparent.png",
+  },
   openGraph: {
     title: "RocketHacks 2026 | Hack the Horizon",
-    description: "Join the biggest hackathon in the Midwest at the University of Toledo",
+    description:
+      "Join the biggest hackathon in the Midwest at the University of Toledo",
     url: "https://rockethacks.org",
     siteName: "RocketHacks",
     images: [
@@ -32,17 +37,18 @@ export const metadata: Metadata = {
         url: "/assets/rh_26/rh_26_folder/rh_26_bundle_jpg/Rockethacks 26 Linkedin Banner.png",
         width: 1200,
         height: 630,
-        alt: "RocketHacks Logo"
-      }
+        alt: "RocketHacks Logo",
+      },
     ],
     locale: "en_US",
-    type: "website"
+    type: "website",
   },
   twitter: {
     card: "summary_large_image",
     title: "RocketHacks 2026 | Hack the Horizon",
-    description: "Join the biggest hackathon in the Midwest at the University of Toledo"
-  }
+    description:
+      "Join the biggest hackathon in the Midwest at the University of Toledo",
+  },
 };
 
 export default function RootLayout({
@@ -57,7 +63,7 @@ export default function RootLayout({
         <a
           id="mlh-trust-badge"
           className="fixed top-0 right-0 w-[50px] sm:w-[65px] md:w-[80px] lg:w-[100px] max-w-[100px] min-w-[50px] z-[45] block transition-transform hover:scale-105 active:scale-95"
-          style={{ display: 'block' }}
+          style={{ display: "block" }}
           href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=yellow"
           target="_blank"
           rel="noopener noreferrer"
@@ -66,7 +72,7 @@ export default function RootLayout({
           <img
             src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-yellow.svg"
             alt="Major League Hacking 2026 Hackathon Season"
-            style={{ width: '100%' }}
+            style={{ width: "100%" }}
             className="filter drop-shadow-lg"
           />
         </a>
